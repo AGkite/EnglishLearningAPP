8500f8f5e9ea4782487f25e33f9f90c7c5ee54d0
AGkite <liuwenhao8888@gmail.com>
2023-10-05 23:02:26 +0800

    搭建springboot后端

---
+++ English-Learning-SpringBoot/english-learning-common/src/test/java/com/howonenew/common/EnglishLearningCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.howonenew.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishLearningCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ English-Learning-SpringBoot/english-learning-web/src/main/java/com/howonenew/web/EnglishLearningWebApplication.java
@@ -0,0 +1,13 @@
+package com.howonenew.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishLearningWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishLearningWebApplication.class, args);
+	}
+
+}
